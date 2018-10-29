20308e2ebf9376ced148b854cf543abe2d070d57
Diegodk <diegodksanchez@gmail.com>
2018-10-29 14:12:10 -0600

    new adapter

---
+++ app/src/main/java/sv/dk/com/dimeunahistoria/Adapters/sectionAdapter.java
@@ -0,0 +1,92 @@
+package sv.dk.com.dimeunahistoria.Adapters;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import sv.dk.com.dimeunahistoria.Modelos.SectionsItem;
+import sv.dk.com.dimeunahistoria.R;
+
+/**
+ * Created by DK-Ragnar on 29/10/2018.
+ */
+
+public class sectionAdapter extends  RecyclerView.Adapter<sectionAdapter.ViewHolder>{
+
+
+    private List<SectionsItem> purposesList;
+    private int layout;
+    private onItemClickListener listener;
+
+    public sectionAdapter(List<SectionsItem> Purposes, int layout, onItemClickListener listener) {
+            this.purposesList = Purposes;
+            this.layout = layout;
+            this.listener = listener;
+            }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+            //inlfamos el layout y le pasamos lso datos al constructor del view holder
+            View v = LayoutInflater.from(parent.getContext()).inflate(layout,parent,false);
+            ViewHolder vh = new ViewHolder(v);
+            return vh;
+            }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+            //llamamos al metodo bind del viewholder pasandole el objdeto y un listener
+            holder.bind(purposesList.get(position), listener);
+
+            }
+
+    @Override
+    public int getItemCount() {
+            return purposesList.size();
+            }
+
+    public static class ViewHolder extends  RecyclerView.ViewHolder{
+        //Elementos UI a rellenar
+
+        ImageView portada;
+
+
+        public ViewHolder(View v){
+
+            //recibe la vista completa para que la rendericemos, pasamos parametros a constructor padre
+            // aqui tambien manejamos los datos de logioca para extraer datos y hacer referencias con los elementoss
+            super(v);
+            portada = (ImageView) itemView.findViewById(R.id.txthistoriaportada);
+
+        }
+
+        public void bind(final SectionsItem sectionsItem, final  onItemClickListener listener){
+            //procesamos los datos para renderizar
+            portada = (ImageView) itemView.findViewById(R.id.txthistoriaportada);
+
+
+
+            Glide.with(portada.getContext()).load("http://ec2-54-244-63-119.us-west-2.compute.amazonaws.com/story/public/images/"+
+                    sectionsItem.getUrl()).into(portada);
+
+            portada.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listener.onItemClick(sectionsItem, getAdapterPosition());
+                }
+            });
+
+        }
+
+    }
+    ///declaramos las interfaces con los metodos a implementar
+    public interface onItemClickListener{
+        void onItemClick(SectionsItem sectionsItem, int position);
+    }
+}
--- app/src/main/java/sv/dk/com/dimeunahistoria/HistoriaDetalle.java
+++ app/src/main/java/sv/dk/com/dimeunahistoria/HistoriaDetalle.java
@@ -4,8 +4,12 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -15,37 +19,76 @@ import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
+import sv.dk.com.dimeunahistoria.Adapters.HistoriasAdapter;
+import sv.dk.com.dimeunahistoria.Adapters.SeccionAdapter;
+import sv.dk.com.dimeunahistoria.Adapters.sectionAdapter;
 import sv.dk.com.dimeunahistoria.Modelos.Historia;
 import sv.dk.com.dimeunahistoria.Modelos.Seccion;
+import sv.dk.com.dimeunahistoria.Modelos.SectionsItem;
 import sv.dk.com.dimeunahistoria.Modelos.Story;
 
-public class HistoriaDetalle extends AppCompatActivity {
+public class HistoriaDetalle extends AppCompatActivity{
 
     Story historia;
     ImageView portada;
     TextView titulo, categoria, autor;
     LinearLayout fondoDetalle;
+    RecyclerView recyclerView;
+    //SeccionAdapter adaptador;
+    sectionAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historia_detalle);
 
+        recyclerView = (RecyclerView) findViewById(R.id.seccionRecycler);
+
         //findViewById
+        /*
         portada = (ImageView) findViewById(R.id.portadaDetalle);
+
         titulo = (TextView) findViewById(R.id.txtTituloDetalle);
         categoria = (TextView) findViewById(R.id.txtCategoriaDetalle);
-        fondoDetalle = (LinearLayout) findViewById(R.id.fondoDetalle);
+        fondoDetalle = (LinearLayout) findViewById(R.id.fondoDetalle);*/
 
         historia = (Story) getIntent().getSerializableExtra("historia");
 
+        /*
         Glide.with(this).load("http://ec2-54-244-63-119.us-west-2.compute.amazonaws.com/story/public/images/"+historia.getUrl()).into(portada);
-        titulo.setText(historia.getName());
+        titulo.setText(historia.getName())seccionRecycler;
         if(historia.getCategory()!= null){
             categoria.setText(historia.getCategory().getName());
-        }
+        }*/
+
+        final LayoutAnimationController controller = AnimationUtils.loadLayoutAnimation(HistoriaDetalle.this, R.anim.layout_animation_fall_down);
+        recyclerView.setLayoutAnimation(controller);
+        recyclerView.scheduleLayoutAnimation();
+        recyclerView.setLayoutManager(new GridLayoutManager(HistoriaDetalle.this, 2));
+        adapter = new sectionAdapter(historia.getSections(), R.layout.item_reciclerview, new sectionAdapter.onItemClickListener() {
+            @Override
+            public void onItemClick(SectionsItem sectionsItem, int position) {
+                SectionsItem section = historia.getSections().get(position);
+
+                Intent intent = new Intent(HistoriaDetalle.this, SeccionView.class);
+                intent.putExtra("historia", historia);
+                //intent.putExtra("lista", historia.getSections());
+                intent.putExtra("pagina", position);
+                if(historia.getSections() == null || historia.getSections().size() == 0) {
+                    Toast.makeText(getApplicationContext(),"Historia sin contenido", Toast.LENGTH_SHORT).show();
+                }else{
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
+
+        recyclerView.setAdapter(adapter);
+
 
     }
+
+
 
     public void leerAhora(View view) {
         Intent intent = new Intent(this, SeccionView.class);
@@ -59,4 +102,5 @@ public class HistoriaDetalle extends AppCompatActivity {
             finish();
         }
     }
+
 }
